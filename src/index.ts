b9590bf13099be99222e77963bd981cc572a9db9
shubham.setia <shubham.setia@webileapps.com>
2025-02-20 18:35:03 +0530

    Add text transformation options to convertDecimalToWords function

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,28 @@
 import { toWords } from 'number-to-words';
 
+
+const applyTextTransform = (text: string, textTransform?: 'none' | 'capitalize' | 'uppercase' | 'lowercase'): string => {
+  switch (textTransform) {
+    case 'capitalize':
+      return text.replace(/\b\w/g, char => char.toUpperCase());
+    case 'uppercase':
+      return text.toUpperCase();
+    case 'lowercase':
+      return text.toLowerCase();
+    default:
+      return text;
+  }
+};
 // Utility function to convert a decimal number to words with exact decimal values
-export const convertDecimalToWords = (num: number): string => {
+export const convertDecimalToWords = (num: number, textTransform?: 'none' | 'capitalize' | 'uppercase' | 'lowercase'): string => {
   const [integerPart, decimalPart] = num.toString().split('.');
   const integerWords = toWords(Number(integerPart));
-  if (!decimalPart) return integerWords;
+  if (!decimalPart) return applyTextTransform(integerWords, textTransform);
   
   const decimalWordsArray = decimalPart.split('').map(digit => toWords(Number(digit)));
   const decimalWords = decimalWordsArray.join(' ');
   
-  return `${integerWords} point ${decimalWords}`;
+  return applyTextTransform(`${integerWords} point ${decimalWords}`, textTransform);
 };
+
+
