bc0bbea337b64461ef2c0cc3f0710a39b8f2510b
shubham.setia <shubham.setia@webileapps.com>
2026-03-09 14:41:31 +0530

    build Preserve What User Passed

--- dist/index.js
+++ dist/index.js
@@ -17,20 +17,18 @@ var applyTextTransform = function (text, textTransform) {
 };
 // Utility function to convert a decimal number to words with exact decimal values
 var convertDecimalToWords = function (num, textTransform) {
-    if (typeof num === 'string') {
-        if (isNaN(Number(num))) {
-            return 'NaN';
-        }
-        num = Number(num);
+    var value = typeof num === "number" ? num.toString() : num;
+    if (isNaN(Number(value))) {
+        return "NaN";
     }
-    var _a = num.toString().split('.'), integerPart = _a[0], decimalPart = _a[1];
+    var _a = value.split("."), integerPart = _a[0], decimalPart = _a[1];
     var integerWords = converter.convert(Number(integerPart));
     if (!decimalPart)
         return applyTextTransform(integerWords, textTransform);
-    var decimalWordsArray = decimalPart
-        .split('')
-        .map(function (digit) { return converter.convert(Number(digit)); });
-    var decimalWords = decimalWordsArray.join(' ');
+    var decimalWords = decimalPart
+        .split("")
+        .map(function (digit) { return converter.convert(Number(digit)); })
+        .join(" ");
     return applyTextTransform("".concat(integerWords, " point ").concat(decimalWords), textTransform);
 };
 exports.convertDecimalToWords = convertDecimalToWords;
