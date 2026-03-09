3ae53c75d342dc0635890301f494d4227e433f13
shubham.setia <shubham.setia@webileapps.com>
2026-03-09 14:03:32 +0530

    Fixes TypeError: Cannot read properties of undefined (reading 'options')

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { ToWords } from 'to-words';
 
-const toWords = new ToWords().convert;
+const converter = new ToWords();
 
-const applyTextTransform = (text: string, textTransform?: 'none' | 'capitalize' | 'uppercase' | 'lowercase'): string => {
+const applyTextTransform = (
+  text: string,
+  textTransform?: 'none' | 'capitalize' | 'uppercase' | 'lowercase'
+): string => {
   switch (textTransform) {
     case 'capitalize':
       return text.replace(/\b\w/g, char => char.toUpperCase());
@@ -15,21 +18,32 @@ const applyTextTransform = (text: string, textTransform?: 'none' | 'capitalize' 
   }
 };
 // Utility function to convert a decimal number to words with exact decimal values
-export const convertDecimalToWords = (num: number|string, textTransform?: 'none' | 'capitalize' | 'uppercase' | 'lowercase'): string => {
-  if(typeof num === 'string') {
-    if(isNaN(Number(num))) {
-      return 'NaN'
+export const convertDecimalToWords = (
+  num: number | string,
+  textTransform?: 'none' | 'capitalize' | 'uppercase' | 'lowercase'
+): string => {
+
+  if (typeof num === 'string') {
+    if (isNaN(Number(num))) {
+      return 'NaN';
     }
     num = Number(num);
   }
+
   const [integerPart, decimalPart] = num.toString().split('.');
-  const integerWords = toWords(Number(integerPart));
+
+  const integerWords = converter.convert(Number(integerPart));
+
   if (!decimalPart) return applyTextTransform(integerWords, textTransform);
-  
-  const decimalWordsArray = decimalPart.split('').map(digit => toWords(Number(digit)));
+
+  const decimalWordsArray = decimalPart
+    .split('')
+    .map(digit => converter.convert(Number(digit)));
+
   const decimalWords = decimalWordsArray.join(' ');
-  
-  return applyTextTransform(`${integerWords} point ${decimalWords}`, textTransform);
+
+  return applyTextTransform(
+    `${integerWords} point ${decimalWords}`,
+    textTransform
+  );
 };
-
-
